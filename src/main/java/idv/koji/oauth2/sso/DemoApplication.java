2fe747d319e7ac2c51d6a24e60fd171b021df902
kojilin <koji.lin@gmail.com>
2015-08-07 14:34:49 +0800

    init commit demo

---
+++ src/main/java/idv/koji/oauth2/sso/DemoApplication.java
@@ -0,0 +1,28 @@
+package idv.koji.oauth2.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class DemoApplication {
+
+  @RequestMapping("/hello")
+  public String hello(OAuth2Authentication authentication) {
+    return "hello:" + authentication.getName();
+  }
+
+  @RequestMapping("/details")
+  public Object details(OAuth2Authentication authentication) {
+    return authentication.getUserAuthentication();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+}
