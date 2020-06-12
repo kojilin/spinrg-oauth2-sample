6831b176066ef3f035499b4ab74b9693d432fb4b
Koji Lin <koji.lin@gmail.com>
2020-06-12 10:39:23 +0900

    Test

--- src/main/java/idv/koji/oauth2/sso/DemoApplication.java
+++ src/main/java/idv/koji/oauth2/sso/DemoApplication.java
@@ -22,6 +22,7 @@ public class DemoApplication {
     return authentication.getUserAuthentication();
   }
 
+  
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
